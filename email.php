98b84160962426b0e5ff72eecb23258993d73c9c
dodiyaashwin1994 <dodiyaashwin1994@gmail.com>
2017-03-16 01:48:00 +0530

    Update email.php

--- email.php
+++ email.php
@@ -1,11 +1,21 @@
+<!-- 
+Form
+Form is used to take train number, user email and journey date from user and stores in three variable  $trainnum, $email and $jrnydate.
+
+Php Function
+Then above three variable are inserted into mysql table 'sms' using insert into query in php.
+
+Javascript
+javascript is used to validate two input variables ($trainnum and $email) in the form.
+-->
+
 <div class="headline">
    <div class="container">
      <div class="col-lg-12 col-xs-12 home">
         <h1>Train Status on Email</h1>
          <!--googleoff: index-->
-           <form name="myForm" method="post" onsubmit="return validateForm()" >
+           <form name="myForm" method="post" onsubmit="return validateForm()" > 
             <div class="row">
-
                  <div class="col-md-4 col-xs-12" style="text-align:left; padding-left:0; padding-right:0">
                     <div class="form-group form-group-lg">
                       <i class="fa fa-map-marker input-icon"></i>
@@ -63,24 +73,24 @@
 <?php
 if(isset($_POST['submit']))
 {
-$trainnum=mysqli_real_escape_string($conn,$_POST['trainnum']);
-$email=mysqli_real_escape_string($conn,$_POST['email']);
-$jrnydate=mysqli_real_escape_string($conn,$_POST['jrnydate']);
-if(!empty($trainnum) && !empty($email) && !empty($jrnydate))
+$trainnum=mysqli_real_escape_string($conn,$_POST['trainnum']);  //stores train number 
+$email=mysqli_real_escape_string($conn,$_POST['email']);        // stores email address of user
+$jrnydate=mysqli_real_escape_string($conn,$_POST['jrnydate']);  // stores journey date
+if(!empty($trainnum) && !empty($email) && !empty($jrnydate))    // check that all 3 conditions are true
   {
-     $sql="INSERT INTO sms (trainnum, email, jrnydate) VALUES('$trainnum','$email','$jrnydate')";
-     if(mysqli_query($conn,$sql))
+     $sql="INSERT INTO sms (trainnum, email, jrnydate) VALUES('$trainnum','$email','$jrnydate')";  //sql query to insert into sms table
+     if(mysqli_query($conn,$sql))   // if query successfully executed then returns true else false
      {
      echo "<div class='container train' style='padding-right: 0 !important; padding-left: 0 !important;'>";
      echo "<div class='row' style='text-align: center; padding: 20px; margin:0;'>";
-        echo "<h3>You Have Successfully Registered</h3>";
+        echo "<h3>You Have Successfully Registered</h3>"; // prints message of successfull registration
      echo "</div>";
      echo "</div>";
      }
      else{
      echo "<div class='container train' style='padding-right: 0 !important; padding-left: 0 !important;'>";
      echo "<div class='row' style='text-align: center; padding: 20px; margin:0;'>";
-        echo "<h3>Failed to Register</h3>";
+        echo "<h3>Failed to Register</h3>"; // prints message of registration failure
      echo "</div>";
      echo "</div>";
      }
@@ -89,23 +99,24 @@ if(!empty($trainnum) && !empty($email) && !empty($jrnydate))
 }
 
 ?>
+<!--form validation-->
 <script>
 function validateForm() {
-    var x = document.forms["myForm"]["trainnum"].value;
-    var y = document.forms["myForm"]["email"].value;
-    var atpos = y.indexOf("@");
+    var x = document.forms["myForm"]["trainnum"].value; //stores value of train number in x
+    var y = document.forms["myForm"]["email"].value;    //stores value of email in y
+    var atpos = y.indexOf("@"); 
     var dotpos = y.lastIndexOf(".");
-    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=y.length) {
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=y.length) {   // checks postion of @ and dot in y
         alert("Not a valid e-mail address");
         return false;
     }
-         if(isNaN(x)||x.indexOf(" ")!=-1)
+         if(isNaN(x)||x.indexOf(" ")!=-1)    // check whether x is interger 
            {
               alert("Please enter trainnum")
               return false; 
            }
            
-           if (x.length !=5)
+           if (x.length !=5)                 // check whether x lenght of x is 5
            {
                 alert("enter 5 digit train number");
                 return false;
